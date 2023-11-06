4315c11b719241fc87773d817a16b907a969a957
丞之 <zmq175@users.noreply.github.com>
2023-11-07 00:54:24 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 #include <tlhelp32.h>
 #include <iostream>
 
-// 用于根据进程名称获取进程ID的函数
 DWORD GetProcessIdByName(const char *processName) {
     PROCESSENTRY32 processInfo;
     processInfo.dwSize = sizeof(processInfo);
@@ -13,13 +12,13 @@ DWORD GetProcessIdByName(const char *processName) {
     }
 
     Process32First(processesSnapshot, &processInfo);
-    if (!strcmp(processName, processInfo.szExeFile)) {
+    if (strcmp(processName, processInfo.szExeFile) == 0) {
         CloseHandle(processesSnapshot);
         return processInfo.th32ProcessID;
     }
 
     while (Process32Next(processesSnapshot, &processInfo)) {
-        if (!strcmp(processName, processInfo.szExeFile)) {
+        if (strcmp(processName, processInfo.szExeFile) == 0) {
             CloseHandle(processesSnapshot);
             return processInfo.th32ProcessID;
         }
@@ -29,29 +28,58 @@ DWORD GetProcessIdByName(const char *processName) {
     return 0;
 }
 
-int main() {
-    // 你要找的进程名称
-    const char *processName = "example.exe";
-    DWORD processID = GetProcessIdByName(processName);
-
-    if (processID == 0) {
-        std::cerr << "Process '" << processName << "' not found." << std::endl;
-        return 1;
-    }
-
-    std::cout << "Process ID of '" << processName << "': " << processID << std::endl;
-
-    // 现在你有了进程ID，你可以用它来打开进程句柄
-    HANDLE processHandle = OpenProcess(PROCESS_VM_READ, FALSE, processID);
+bool WriteMemory(DWORD processID, LPVOID address, const void* data, SIZE_T size) {
+    HANDLE processHandle = OpenProcess(PROCESS_VM_OPERATION | PROCESS_VM_WRITE, FALSE, processID);
     if (processHandle == NULL) {
         std::cerr << "OpenProcess failed: " << GetLastError() << std::endl;
+        return false;
+    }
+
+    BOOL result = WriteProcessMemory(processHandle, address, data, size, NULL);
+    if (result == FALSE) {
+        std::cerr << "WriteProcessMemory failed: " << GetLastError() << std::endl;
+    } else {
+        std::cout << "Memory written successfully" << std::endl;
+    }
+
+    CloseHandle(processHandle);
+    return result != FALSE;
+}
+
+int main() {
+    const char *processName = "VALORANT.exe"; 
+    LPCVOID targetAddress = (LPCVOID)0x484f4f5849; 
+    int valueToWrite = 21195828; // 替换为特殊值
+
+    DWORD processID = GetProcessIdByName(processName);
+    if (processID == 0) {
+        std::cerr << "Process not found." << std::endl;
         return 1;
     }
 
-    // 这里可以添加读取进程内存的代码...
+    std::cout << "Process ID: " << processID << std::endl;
 
-    // 关闭句柄
-    CloseHandle(processHandle);
+    if (WriteMemory(processID, (LPVOID)targetAddress, &valueToWrite, sizeof(valueToWrite))) {
+        std::cout << "Value written successfully." << std::endl;
+    }
+
+    HWND hWnd_Target = FindWindow(NULL, TEXT("VALORANT")); 
+    HDC hDC_Target = GetDC(hWnd_Target);
+
+    HPEN hPen = CreatePen(PS_SOLID, 2, RGB(255, 255, 255));
+    SelectObject(hDC_Desktop, hPen);
+
+    // 设置填充为透明
+    SetBkMode(hDC_Desktop, TRANSPARENT);
+
+    // 绘制三个白色方框
+    Rectangle(hDC_Desktop, 100, 100, 200, 200);
+    Rectangle(hDC_Desktop, 300, 100, 400, 200);
+    Rectangle(hDC_Desktop, 500, 100, 600, 200);
+
+    // 释放设备上下文
+    ReleaseDC(NULL, hDC_Desktop);
+    DeleteObject(hPen);
 
     return 0;
 }
